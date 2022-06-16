6e38d8c3383fa621bab320a9bfbe716061deb142
Slimani Louoizani <slimanilouoizani@MBP-de-Loizani.localdomain>
2022-06-16 16:50:29 +0200

    sauvegarde rmi serveur partage de saisie entre client ok : manque pull

--- src/main/java/RMI/RMIInterfaceIMPL.java
+++ src/main/java/RMI/RMIInterfaceIMPL.java
@@ -3,14 +3,38 @@ package RMI;
 
 import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.SortedSet;
+import java.util.TreeSet;
 
 
 public class RMIInterfaceIMPL extends UnicastRemoteObject  implements RMIInterface {
 	
 	private static final long serialVersionUID = 1L;
 	
+	private ArrayList<String> objSetJeuSaisies = null ;
+	
+	//private collection colll ;
+	
 	protected RMIInterfaceIMPL() throws RemoteException {
 		super(1097);
+	}
+
+	//ArrayList<String> cars = new ArrayList<String>();
+	
+	@Override
+	public ArrayList<String>  stoquerSaisieClient(String sArgSaisie) throws RemoteException {
+		System.out.println("(<stoquerSaisieClient hashcode de l\'instance serveur = "+this.hashCode()+"> Ici serveur) : saisie client vaut \"" + sArgSaisie + "\"");				
+		
+		if (objSetJeuSaisies == null)
+			objSetJeuSaisies = new ArrayList<String>();
+		objSetJeuSaisies.add(sArgSaisie);
+		
+		System.out.println("(<stoquerSaisieClient objSetJeuSaisies membre, son hashcode = "+objSetJeuSaisies.hashCode()+">");
+
+		return objSetJeuSaisies ;
 	}
 
 	@Override
@@ -19,15 +43,9 @@ public class RMIInterfaceIMPL extends UnicastRemoteObject  implements RMIInterfa
 	}
 
 	@Override
-	public String StockerEnStatiqueIpClient(interfClient intClient) throws RemoteException {
-	    System.out.println("≤StockerEnStatiqueIpClient> : dans l'implementation serveur");
-		System.out.println("≤StockerEnStatiqueIpClient> intClient.hashCode(): " +  intClient.hashCode()) ;
-        System.out.println("≤StockerEnStatiqueIpClient> intClient.RecupereAdresseIp() : " +  intClient.RecupereAdresseIp() ) ;
-        System.out.println("≤StockerEnStatiqueIpClient> intClient.getAttAdresseIp() : " +  intClient.getAttAdresseIp()) ;		
+	public String StockerEnStatiqueIpClient(interfClient intClient) throws RemoteException {	
 		String sHote = intClient.RecupereAdresseIp() ;		 
 	    return sHote ; 
-		//return "ceci est la réponse du serveur (au niveau du serveur)" ;
 	}
-
 
 }
--- src/main/java/RMI/ServerOperation.java
+++ src/main/java/RMI/ServerOperation.java
@@ -1,29 +1,19 @@
 package RMI;
 
-import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 
 public class ServerOperation {	
     
-    static String DonneeClient ;  
-    
-    public String helloTo(String dataclient) throws RemoteException {
-    	ServerOperation.DonneeClient = dataclient ; 
-        System.err.println(dataclient + " : valeur envoyée du client") ; 
-        return "Le serveur a bien reçu le mot : " + dataclient ; 
-    }
     
     public static void main(String[] args){    	
         try {        	 
-        	System.setProperty("java.security.policy","file:java.policy") ; 
-        	System.setProperty("java.rmi.server.hostname","192.168.6.107");
+        	/*System.setProperty("java.security.policy","file:java.policy") ; 
+        	System.setProperty("java.rmi.server.hostname","192.168.6.107"); */
         	RMIInterfaceIMPL rmil = new RMIInterfaceIMPL()  ;         	
         	Registry varRegistry = LocateRegistry.createRegistry(1097) ;
         	varRegistry.rebind("loizrmiserver", rmil );
     		System.out.println("jesuis le serveur > après rebind");
-         	//varRegistry = java.rmi.registry.LocateRegistry.createRegistry(1097) ;  
-         	//varRegistry.bind("loizrmiserver", new ServerOperation());
          	
         } 
         catch (Exception e) {
